ab3fa852c365c7f78e00a1889fe63046a84f9bc9
Calvin Katt <katt.calvin@gmail.com>
2021-04-03 19:59:28 +0200

    Fixed all bugs in new and delete

--- kernel/src/memory/Heap/Heap.cpp
+++ kernel/src/memory/Heap/Heap.cpp
@@ -1,8 +1,24 @@
 #include "Heap.h"
 #include "../PMM/PageFrameAllocator.h"
 #include "../VMM/VirtualMemoryManager.h"
+struct Heap{
+    uint64_t begin;
+    uint64_t end;
+    uint64_t size;
+    Heap(uint64_t b, uint64_t e, uint64_t sz);
+    Heap();
+};
 
-constexpr uint64_t HeapEntryHeaderMagicValue = 0xe1efa87e1efa87;
+
+//Used for Free List
+struct HeapEntryHeader{
+    uint64_t magicValue = 0xe1efa87e1efa87;
+    uint64_t size;
+    HeapEntryHeader* nextHeader;
+    HeapEntryHeader* previousHeader;
+    HeapEntryHeader(HeapEntryHeader* next, HeapEntryHeader* prev, uint64_t sz);
+};
+constexpr uint64_t HeapEntryHeaderMagicValue = 0xe1efa87e1efa87ff;
 Heap kernelHeap;
 HeapEntryHeader* startSec;
 HeapEntryHeader* head;
@@ -44,7 +60,9 @@ bool growHeap(){
 }
 
 void* operator new(size_t sz){
-    sz += 8 - (sz%8);//Blocks are 8 Byte aligned
+    if(sz%8 != 0){
+        sz += 8 - (sz%8);//Blocks are 8 Byte aligned
+    }
     HeapEntryHeader* seg = findFreeSegment(sz);
     while(seg == nullptr){
         bool suc = growHeap();
@@ -63,20 +81,20 @@ void* operator new(size_t sz){
         seg->previousHeader = nullptr;
         return reinterpret_cast<void*>(reinterpret_cast<uint64_t>(seg + sizeof(HeapEntryHeader)));
     } else {
-        seg->size -= sizeof(HeapEntryHeader);
         HeapEntryHeader* newHidden = reinterpret_cast<HeapEntryHeader*>(reinterpret_cast<uint64_t>(seg) + seg->size);
+        seg->size -= sizeof(HeapEntryHeader);
         newHidden->magicValue = HeapEntryHeaderMagicValue;
         newHidden->size = sz;
         newHidden->nextHeader = nullptr;
         newHidden->previousHeader = nullptr;
-        return reinterpret_cast<void*>(reinterpret_cast<uint64_t>(newHidden + sizeof(HeapEntryHeader)));
+        return reinterpret_cast<void*>(reinterpret_cast<uint64_t>(newHidden) + sizeof(HeapEntryHeader));
         //*newHidden = HeapEntryHeader(nullptr,)
     }
 }
 void* operator new[](size_t sz){
     return operator new(sz);
 }
-void operator delete(void* p){
+void operator delete(void* p,uint64_t sz){
     HeapEntryHeader* seg = reinterpret_cast<HeapEntryHeader*>(reinterpret_cast<uint64_t>(p) - sizeof(HeapEntryHeader));
     if(seg->magicValue != HeapEntryHeaderMagicValue){
         //TODO: Panic
@@ -85,6 +103,9 @@ void operator delete(void* p){
     HeapEntryHeader* node = head;
     while (reinterpret_cast<uint64_t>(node) < reinterpret_cast<uint64_t> (seg)){
         node = node->nextHeader;
+        if (node == head){
+            break;
+        }
     }
     HeapEntryHeader* prev = node->previousHeader;
     //Check for unification
@@ -112,5 +133,5 @@ void operator delete(void* p){
 }
 
 void operator delete[](void* p){
-    operator delete(p);
+    operator delete(p,0);
 }
