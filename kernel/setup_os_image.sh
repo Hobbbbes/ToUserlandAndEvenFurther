17bc095019f5fc30c7c5e7c2b8a75ec86954f23b
Calvin Katt <katt.calvin@gmail.com>
2021-07-21 13:34:33 +0200

    working image infastructure

--- kernel/setup_os_image.sh
+++ kernel/setup_os_image.sh
@@ -7,8 +7,7 @@ if ! [ -f "$BUILDDIR/$OSNAME.img" ]; then
     dd if=/dev/zero of=$BUILDDIR/$OSNAME.img bs=4096 count=100000
     sgdisk --clear --new 1:2048:98304 --new 2:100352:799933 -t 1:EF00 -t 2:8300 $BUILDDIR/$OSNAME.img
 fi
-
-
-if ! [ -f "/dev/($OSNAME)_loop" ]; then
-    export ($OSNAME)_loop=losetup --partscan --show --find "$BUILDDIR/$OSNAME.img" "/dev/($OSNAME)_loop" 
+if ! [ -f `cat loopdevice` ]; then 
+    losetup --partscan --show --find "$BUILDDIR/$OSNAME.img" > loopdevice
+    partprobe `cat loopdevice` || true
 fi
